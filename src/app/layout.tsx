481a04004aa94a1e2e9a1a77ba735216b1867db5
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2026-03-15 23:42:02 +0600

    added: [custom zingy logo]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
+import Header from "@/components/sections/header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const alpino = localFont({
+  src: "../../public/fonts/Alpino-Variable.woff2",
+  display: "swap",
+  weight: "100 900",
+  variable: "--font-alpino",
 });
 
 export const metadata: Metadata = {
@@ -24,10 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${alpino.variable} font-sans antialiased`}>
+        <Header />
+        <main>{children}</main>
       </body>
     </html>
   );
---
+++ src/components/sections/header.tsx
@@ -0,0 +1,11 @@
+import { ZingyLogo } from "@/components/zingy-logo";
+
+const Header = () => {
+  return (
+    <header className="-mb-28 flex justify-center py-4">
+      <ZingyLogo />
+    </header>
+  );
+};
+
+export default Header;
