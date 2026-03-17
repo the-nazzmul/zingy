214c881ea670ac671ec36ad303077f7e950fd701
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2026-03-18 01:54:38 +0600

    animation: [finished animating text contents in the hero section]

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -6,8 +6,9 @@ import clsx from "clsx";
 import { TextSplitter } from "@/components/text-splitter";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 const Hero = () => {
   useGSAP(() => {
@@ -38,6 +39,36 @@ const Hero = () => {
         y: 10,
         opacity: 0,
         duration: 0.6,
+      });
+
+    const scrollTL = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".hero",
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1.5,
+      },
+    });
+
+    scrollTL
+      .fromTo(
+        "body",
+        { backgroundColor: "#FDE047" },
+        { backgroundColor: "#D9F99D", overwrite: "auto" },
+        1,
+      )
+      .from(".text-side-heading .split-char", {
+        scale: 1.3,
+        y: 40,
+        rotate: -25,
+        opacity: 0,
+        stagger: 0.1,
+        ease: "back.out(3)",
+        duration: 0.5,
+      })
+      .from(".text-side-body", {
+        y: 20,
+        opacity: 0,
       });
   });
 
