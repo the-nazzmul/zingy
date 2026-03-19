441d6652aeee0abf232a81282ab10bacd86f218e
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2026-03-19 23:14:28 +0600

    [added: Media query hook to render canvas only on desktop and fire up gsap animation only on desktop]

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -11,15 +11,18 @@ import { View } from "@react-three/drei";
 import Scene from "@/components/scenes/scene";
 import { Bubbles } from "@/components/bubbles";
 import { useStore } from "@/hooks/useStore";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 const Hero = () => {
   const ready = useStore((state) => state.ready);
 
+  const isDesktop = useMediaQuery("(min-width: 768px)", true);
+
   useGSAP(
     () => {
-      if (!ready) return;
+      if (!ready && isDesktop) return;
 
       const introTL = gsap.timeline();
 
@@ -80,15 +83,17 @@ const Hero = () => {
           opacity: 0,
         });
     },
-    { dependencies: [ready] },
+    { dependencies: [ready, isDesktop] },
   );
 
   return (
     <Bounded className="hero opacity-0">
-      <View className="hero-scene pointer-events-none sticky top-0 z-50 -mt-[100vh] hidden h-screen w-screen md:block">
-        <Scene />
-        <Bubbles count={300} speed={2} repeat={true} />
-      </View>
+      {isDesktop && (
+        <View className="hero-scene pointer-events-none sticky top-0 z-50 -mt-[100vh] hidden h-screen w-screen md:block">
+          <Scene />
+          <Bubbles count={300} speed={2} repeat={true} />
+        </View>
+      )}
       <div className="grid">
         <div className="grid h-screen place-content-center">
           <div className="grid auto-rows-min place-items-center text-center">
