479caac9343a3830ea6ec31e72398fb293ac061e
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2026-03-22 21:27:20 +0600

    bugfix: [alternating text section made mobile friendly]

--- src/components/sections/alternating-text.tsx
+++ src/components/sections/alternating-text.tsx
@@ -1,15 +1,16 @@
 "use client";
 
-import { ALTERNATING_TEXT } from "@/lib/constants";
 import { Bounded } from "@/components/bounded";
-import { View } from "@react-three/drei";
 import AlternatingTextScene from "@/components/scenes/alternating-text-scene";
+import { ALTERNATING_TEXT } from "@/lib/constants";
+import { View } from "@react-three/drei";
+import clsx from "clsx";
 
 const AlternatingText = () => {
   return (
     <Bounded className="alternating-text-container relative bg-yellow-300 text-sky-950">
       <div>
-        <div className="relative grid">
+        <div className="relative z-100 grid">
           {/* view goes here */}
           <View className="alternating-text-view absolute top-0 left-0 h-screen w-full">
             <AlternatingTextScene />
@@ -21,7 +22,10 @@ const AlternatingText = () => {
               className="alternating-section grid h-screen place-items-center gap-x-12 md:grid-cols-2"
             >
               <div
-                className={index % 2 === 0 ? "col-start-1" : "md:col-start-2"}
+                className={clsx(
+                  index % 2 === 0 ? "col-start-1" : "md:col-start-2",
+                  "rounded-lg p-4 backdrop-blur-lg max-md:bg-white/20",
+                )}
               >
                 <h3 className="text-6xl font-bold text-balance">
                   {item.heading}
